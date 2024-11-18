a930a69ca740b6398342d4f3a5900a486365b3e2
Deepak Chourasia <dchouras@redhat.com>
2024-11-18 12:31:04 +0530

    trying renovate

--- restore.sh
+++ restore.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-for i in $(gh pr list | awk '{print $1}');
-do
-    gh pr edit $i -t old
-    gh pr close $i -d
-done
-
-
-cp -r /home/dchouras/RHODS/DevOps/FBC/operator-back/* .
+#for i in $(gh pr list | awk '{print $1}');
+#do
+#    gh pr edit $i -t old
+#    gh pr close $i -d
+#done
+#
+#
+#cp -r /home/dchouras/RHODS/DevOps/FBC/operator-back/* .
 git add .
 git status
-git commit -m "changes"
+git commit -m "trying renovate"
 git push origin main
